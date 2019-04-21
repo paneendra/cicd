ef0d51864e92e4a71bb0953458004fb731e5491f
paneendra <paneendra.kumar@inhcape.com>
2019-04-21 12:22:24 +0100

    dev inprogress

--- GetAnypointToken.groovy
+++ GetAnypointToken.groovy
@@ -33,8 +33,13 @@ static def doRESTHTTPCall(urlString, method, payload, headers)
 }
 
 //Gets the anypoint platform token
-def getAnypointToken(Properties props)
+def getAnypointToken()
 {
+    Properties props = new Properties()
+    File propsFile = new File('anypoint-platform.properties')
+    propsFile.withInputStream {
+        props.load(it)
+    }
     //log(DEBUG,  "START getAnypointToken")
     def username=props.getProperty('anypoint.platform.user')
     def password=props.getProperty('anypoint.platform.password')
@@ -62,10 +67,5 @@ def getAnypointToken(Properties props)
     return token
 }
 
-Properties props = new Properties()
-File propsFile = new File('anypoint-platform.properties')
-propsFile.withInputStream {
-    props.load(it)
-}
 
 //String token = getAnypointToken(props)
